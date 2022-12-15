f9543f27e19ba2c12e366906709c0eaaf3ac885f
Axelb2b <brunelaxel@gmail.com>
2022-12-15 15:15:16 +0100

    ismalus() , isbonus()

--- utils.js
+++ utils.js
@@ -5,3 +5,13 @@ function randomIntFromInterval(min, max) {
 function rollDice() {
     return Math.floor(Math.random() * 6) + 1;
 }
+
+ //Fonction qui renvoie un booléen selon si la case est un malus
+ function isMalus(currentPos){
+    return malus.includes(currentPos);
+}
+
+//Fonction qui renvoie un booléen selon si la case est un bonus
+function isBonus(currentPos){
+    return bonus.includes(currentPos);
+}
